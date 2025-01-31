e6434bfc38bc49a2294760fa6cd153c4bd7b3855
Prasert Saengkaew <kengsert@gmail.com>
2025-01-31 14:22:19 +0700

    change resume url

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
               <ButtonPrimary
               label="Download CV"
               icon="download"
-              href="https://shorturl.at/raRWa"
+              href="https://shorturl.at/OCJJN"
               target="_blank"
               />
                <ButtonPrimary
